75aac1a3f07f5eb2f8337b72142d6dc03e398478
suryaprakash <munavathsurya@gmail.com>
2025-05-17 17:59:50 +0530

    echo,print

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 echo "Hello,Im munavath Surya Prakash"
-echo "Im learning aws devops
-echo "shiva kumar reddy, joindevops.com"
+echo "Im learning aws devops"
+echo "shiva kumar reddy,joindevops.com"
