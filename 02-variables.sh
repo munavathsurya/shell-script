70a17b8dbcaef72e4e6dac03dc207b2515c91df1
suryaprakash <munavathsurya@gmail.com>
2025-05-17 19:18:48 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "surya Murali Naik Bhukya is an Indian politician from Telangana state. He is an MLA from Mahabubabad Assembly constituency which is reserved for ST community Mahabubabad district. He represents Indian National Congress Party and won the 2023 Telangana Legislative Assembly election"
-echo "prakash Naik is from Mahabubabad. He was born to Mangya Naik Bhukya in 1966. He is a medical doctor. He also married a doctor. He did his M.B.B.S. at Osmania Medical College in 1994.[4] Later, he completed his Master of Surgery from Pondicherry University, JIPMER in 2004.[5]"
+echo "surya Murali Naik Bhukya is an Indian politician from Telangana state?"
+echo "prakash He was born to Mangya Naik Bhukya in 1966?"
