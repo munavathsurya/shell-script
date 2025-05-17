e5afcb7522e76513b94c65d6bc101022663b3b17
suryaprakash <munavathsurya@gmail.com>
2025-05-17 19:26:09 +0530

    variablesecho

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "surya Murali Naik Bhukya is an Indian politician from Telangana state?"
-echo "prakash He was born to Mangya Naik Bhukya in 1966?"
+echo "Surya:: Murali Naik Bhukya is an Indian politician from Telangana state?"
+echo "Prakash:: He was born to Mangya Naik Bhukya in 1966?"
