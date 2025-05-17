622dae1227f36b30bfe40f50012099922ab779e2
suryaprakash <munavathsurya@gmail.com>
2025-05-17 18:54:23 +0530

    variables echo

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Ramesh:: hai suresh. how are you?"
+echo "Ramesh: hai suresh. how are you?"
 echo "Suresh:: hai ramesh. iam fine, how are you?"
 echo "Ramesh:: iam fine too. how is your work?"
 echo "Suresh:: iam thinking to upgrade aws devops?"
