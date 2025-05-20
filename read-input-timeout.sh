e1af0ce457df310ba95eda6d4b9e48ced8731ea3
suryaprakash <munavathsurya@gmail.com>
2025-05-20 23:10:39 +0530

    input

--- read-input-timeout.sh
+++ read-input-timeout.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-read -t 10 -p"enter your password within 10 seconds:"
+read -t 10 -p"enter your password within 10 seconds:" password
 if {-z "$password"}; than
 echo "no password entered within 10 seconds."
-else 
+
+else
+
 echo "password entered:$password"
 fi
