879b3d6d2e36115361f6e94fb55d35c46a62852a
suryaprakash <munavathsurya@gmail.com>
2025-05-21 11:57:09 +0530

    string operatin

--- string-operations.sh
+++ string-operations.sh
@@ -7,5 +7,9 @@ if { "$str1" = "$str2' }; then
 echo " strings are equal "
 fi
 if {"$str1" != "$str2"}; then
-echo "string are not equal"
+echo "string are not equal";
 fi
+str3="$str1 $str2"
+echo "concentenstedd string: $str3"
+length=${#str1}
+echo "length of str1 is $length"
