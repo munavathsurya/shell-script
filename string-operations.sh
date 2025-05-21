c869b3ac66634c38430ffd0c455071dba9856bdb
suryaprakash <munavathsurya@gmail.com>
2025-05-21 11:50:47 +0530

    string operationsa

--- string-operations.sh
+++ string-operations.sh
@@ -4,5 +4,8 @@ str2="world"
 echo "string operations:"
 echo "================="
 if { "$str1" = "$str2' }; then
-echo "strings are equal"
+echo " strings are equal "
+fi
+if {"$str1" != "$str2"}; then
+echo "string are not equal"
 fi
