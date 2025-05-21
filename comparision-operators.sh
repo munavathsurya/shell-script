ccc28ef613d6875f06f20396d4e8d2c8c4959221
suryaprakash <munavathsurya@gmail.com>
2025-05-21 11:02:49 +0530

    comparison

--- comparision-operators.sh
+++ comparision-operators.sh
@@ -1,5 +1,12 @@
 a=20
 b=30
 echo "comparison operators:"
-if { $a -eq $b }; than
+if { $a -eq $b }; then
 echo "a is equal to b"
+ fi
+if { $a -ne $b }; then
+echo "a is not equal to b"
+ fi
+if {$a -gt $b }; then 
+echo "a is greater then equal b"
+ fi
